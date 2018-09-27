2071d663adb1cada3ef020cf3e485f4baa6d3149
Tuấn Vương <vanhtuan0409@gmail.com>
2018-09-27 10:55:31 +0700

    add cli arg parser

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,12 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	chat "github.com/vanhtuan0409/go-chat"
+	cli "gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	client := chat.NewClient("tuanva", "localhost", 8080)
-	log.Fatal(client.Connect())
+	app := cli.NewApp()
+	app.Name = "Go Chat Client"
+	app.Usage = "Client for Chat application"
+	app.Version = "v0.1.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "name, n",
+			Value: "",
+			Usage: "User name when chat. Should be unique",
+		},
+		cli.StringFlag{
+			Name:  "address, a",
+			Value: "localhost",
+			Usage: "Address name of server",
+		},
+		cli.IntFlag{
+			Name:  "port, p",
+			Value: 8080,
+			Usage: "Runing port of server",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		name := c.String("name")
+		if name == "" {
+			fmt.Println("require name value")
+			return
+		}
+
+		address := c.String("address")
+		if address == "" {
+			fmt.Println("require address value")
+			return
+		}
+
+		port := c.Int("port")
+		if port <= 0 {
+			fmt.Println("require port value")
+			return
+		}
+
+		client := chat.NewClient(name, address, port)
+		log.Fatal(client.Connect())
+	}
+
+	app.Run(os.Args)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	chat "github.com/vanhtuan0409/go-chat"
-)
-
-const (
-	ServerPort = 8080
+	cli "gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	server := chat.NewServer(ServerPort)
+	app := cli.NewApp()
+	app.Name = "Go Chat Server"
+	app.Usage = "Server for Chat application"
+	app.Version = "v0.1.0"
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "port, p",
+			Value: 8080,
+			Usage: "Runing port of server",
+		},
+	}
+	app.Action = handle
+	app.Run(os.Args)
+}
+
+func handle(c *cli.Context) {
+	port := c.Int("port")
+	if port <= 0 {
+		fmt.Println("require port value")
+		return
+	}
+
+	server := chat.NewServer(port)
 	sigTerm := make(chan os.Signal)
 
 	go func() {
 		log.Fatal(server.Listen())
 	}()
-	log.Printf("Server is running on :%d\n", ServerPort)
+	log.Printf("Server is running on :%d\n", port)
 
 	signal.Notify(sigTerm, syscall.SIGTERM, syscall.SIGINT)
 	<-sigTerm
